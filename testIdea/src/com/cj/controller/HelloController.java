d6253e064dfdbb82d5334cb9821b2e46d447eeb9
moyu <@moyu.com>
2021-01-08 21:14:12 +0800

    修改文件

---
+++ testIdea/src/com/cj/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.cj.controller;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * @Classname HelloControllrt
+ * @Title:
+ * @Description:TODO
+ * @author:moyu
+ * @date 2021-01-08 21:11
+ * @version:1.0
+ */
+public class HelloController {
+
+    public void say(){
+        System.out.println("hello");
+    }
+}
